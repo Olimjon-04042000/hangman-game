fc580ab34aa3c28794b2f362db0c5b3157f3f5da
Olimjon-04042000 <nishonovolimjon2000@gmail.com>
2024-03-04 09:07:41 +0500

    first commit

--- src/Letters.jsx
+++ src/Letters.jsx
@@ -1,16 +1,60 @@
-
+import React, { useState } from "react";
 
 const Letters = () => {
+  const [disable, setDisabled] = useState([]);
 
-  const lettersArr=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  const handleClick = (event) => {
+    {
+      console.log(event.target.textContent);
+    }
+    const letter = event.target.textContent;
+
+    setDisabled([...disable, letter]);
+  };
+
+  const lettersArr = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
 
   return (
     <div className="container letters">
-      {lettersArr.map((letter)=>(
-     <button key={letter} className="letter">{letter}</button>
+      {lettersArr.map((letter) => (
+        <button
+          onClick={handleClick}
+          key={letter}
+          className="letter"
+          disabled={disable.includes(letter)}
+        >
+          {letter}
+        </button>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default Letters
+export default Letters;
--- src/RandomCountry.jsx
+++ src/RandomCountry.jsx
@@ -1,13 +1,20 @@
 import data from "./data";
 
 const RandomCountry = () => {
-  let randomNumber=Math.trunc(Math.random()*31);
-  const country=data[randomNumber].name;
+  let randomNumber = Math.trunc(Math.random() * 31);
+  const country = data[randomNumber].name;
+  const countryLetters = country.split("");
   return (
-    <div className='container'>
-     {country}
+    <div className="container">
+      <div className="letter-box">
+        {countryLetters.map((countryLetter, index) => (
+          <div key={index} className="counrty-letter">
+            {countryLetter}
+          </div>
+        ))} 
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default RandomCountry
+export default RandomCountry;
